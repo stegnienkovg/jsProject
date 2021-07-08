e2eb57294248c063326adf7c8588ab7a691b3f0a
Veronika <stegnienkovg@gmail.com>
2021-07-08 16:01:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+(function() {
+    'use strict';
+}());
+
+
+
+// let number = 5;
+// const leftBorderWith = '1';
+
+// number = 10;
+// console.log(number);
+
+// const car = {
+//     type: 'cupe',
+//     color: 'black',
+//     isNew: true
+// };
+// console.log(car.color);
+
+// const arr = ['cupe', 'black', true, 4];
+// console.log(arr[2]);
+
+
+// alert('Беда беда Мама лошара');
+
+// const conf = confirm('ты мужчина?');
+
+// const qw = prompt('Сколько вам лет?', '');
+// console.log(conf + ' ' + qw);
+
+
+const name = prompt('Ваше имя?', '');
+
+console.log(`Hello, ${name}`);
+
+
+
+const isCecked = true,
+    isClosed = false;
+
+console.log(!isClosed);
+
+console.log(5 == 6 - 1);
+console.log(2 + 3 * 2 === '8')
